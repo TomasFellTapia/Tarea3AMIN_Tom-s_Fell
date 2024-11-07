804c1192609a5a50318c0eb05226392350a11b1a
TomasFellTapia <111521118+TomasFellTapia@users.noreply.github.com>
2024-11-07 11:59:30 -0300

    fds
    fsdfs

---
+++ Optimizacion/Funciones/Funciones01.py
@@ -0,0 +1,13 @@
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+def valores(archivo):
+    
+    df = pd.read_table(archivo, header=None)
+    n = int(df[0][1].split()[1])
+    c = int(df[0][2].split()[1])
+    z = int(df[0][3].split()[1])
+    df.drop(df.index[0:5],axis=0,inplace=True)
+    df.drop(df.tail(1).index,axis=0,inplace=True)
+    df = df[0].str.split
+    print ("N= ",n,"; C= ",c,"; Z= ",z)
+    print(df)
--- Optimizacion/Knapsack.py
+++ Optimizacion/Knapsack.py
@@ -2,10 +2,20 @@ import sys
 import numpy as np # type: ignore
 import pandas as pd # type: ignore
 import time 
-from Funciones.Funciones01 import 
+from Funciones.Funciones01 import *
 if len(sys.argv)== 5:
-    sem = int(sys.argv[1])
+    nom = str(sys.argv[1])
+    sem = int(sys.argv[2])
+    numit = int(sys.argv[3])
+    tau = float(sys.argv[4])
+    np.random.seed(sem)
+    nom = "./Entradas/"+nom+".csv"
+    n = 50
+    print(nom)
 
+    vectini = np.random.randint(2,size=n)
+    valores(nom)
+    print (vectini)
 
 else: 
     print("Error en el ingreso de datos")
